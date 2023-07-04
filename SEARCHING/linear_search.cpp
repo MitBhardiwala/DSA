162741d72edf34a5743b5d814f7a02e5b1b857a5
Mit0921 <meetbhardiwala@gmail.com>
2023-07-04 22:40:01 +0530

    This is a linear search program

---
+++ SEARCHING/linear_search.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int temp;
+
+    cout<<"Enter number to be searched :";
+    cin>>temp;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]==temp){
+            cout<<"Number found at index :"<<i<<endl;
+            break;
+        }
+    }
+    return 0;
+}
